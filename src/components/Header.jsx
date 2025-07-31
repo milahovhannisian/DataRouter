0dfa3f6eda34260954239da5437459cd9f559860
Milena Hovhannisyan <196185742+milahovhannisian@users.noreply.github.com>
2025-07-31 18:11:56 +0400

    Removed the jsx code spaces from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 const Header = () => (
   <header>
     <nav>
-      <NavLink to="/posts">Posts</NavLink> |{" "}
-      <NavLink to="/comments">Comments</NavLink> |{" "}
-      <NavLink to="/albums">Albums</NavLink> |{" "}
-      <NavLink to="/photos">Photos</NavLink> |{" "}
-      <NavLink to="/todos">Todos</NavLink> |{" "}
+      <NavLink to="/posts">Posts</NavLink> 
+      <NavLink to="/comments">Comments</NavLink> 
+      <NavLink to="/albums">Albums</NavLink> 
+      <NavLink to="/photos">Photos</NavLink> 
+      <NavLink to="/todos">Todos</NavLink> 
       <NavLink to="/users">Users</NavLink>
     </nav>
   </header>
